8efbe17dbd5edf4b648ca27c09be173bd1ca195c
Nicolai Thomassen <nicolai-thomassen@hotmail.com>
2022-12-02 19:43:27 +0100

    add tests with input

--- src/day_1.rs
+++ src/day_1.rs
@@ -31,13 +31,20 @@ pub mod day_1 {
     }
 
     #[test]
-    fn test_day_1_part_1() {
+    fn test_day_1_part_1_example() {
         let expect = 24000;
-
         let got = day_1_part_1(create_reader_from_file(
             "./src/day_1/example.txt".to_owned(),
         ));
+        assert_eq!(got, expect);
+    }
 
+    #[test]
+    fn test_day_1_part_1_input() {
+        let expect = 69912;
+        let got = day_1_part_1(create_reader_from_file(
+            "./src/day_1/input.txt".to_owned(),
+        ));
         assert_eq!(got, expect);
     }
 
@@ -67,11 +74,22 @@ pub mod day_1 {
     }
 
     #[test]
-    fn test_day_1_part_2() {
+    fn test_day_1_part_2_example() {
         let expect = 45000;
 
         let got = day_1_part_2(create_reader_from_file(
             "./src/day_1/example.txt".to_owned(),
+        ));
+
+        assert_eq!(got, expect);
+    }
+
+    #[test]
+    fn test_day_1_part_2_input() {
+        let expect = 208180;
+
+        let got = day_1_part_2(create_reader_from_file(
+            "./src/day_1/input.txt".to_owned(),
         ));
 
         assert_eq!(got, expect);
