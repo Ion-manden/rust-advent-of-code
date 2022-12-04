98e9efb5296eb93e7dc741ae2586fa65f7ebfd82
Nicolai Thomassen <nicolai-thomassen@hotmail.com>
2022-12-04 10:58:23 +0100

    day 4

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 mod reader;
 
 use day_1::day_1::{day_1_part_1, day_1_part_2};
 use day_2::day_2::{day_2_part_1, day_2_part_2};
 use day_3::day_3::{day_3_part_1, day_3_part_2};
+use day_4::day_4::{day_4_part_1, day_4_part_2};
 use reader::reader::create_reader_from_file;
 
 fn main() {
@@ -32,5 +34,13 @@ fn main() {
     println!(
         "Day 3 Part 2:{}",
         day_3_part_2(create_reader_from_file("./src/day_3/input.txt".to_owned()))
+    );
+    println!(
+        "Day 4 Part 1:{}",
+        day_4_part_1(create_reader_from_file("./src/day_4/input.txt".to_owned()))
+    );
+    println!(
+        "Day 4 Part 2:{}",
+        day_4_part_2(create_reader_from_file("./src/day_4/input.txt".to_owned()))
     );
 }
